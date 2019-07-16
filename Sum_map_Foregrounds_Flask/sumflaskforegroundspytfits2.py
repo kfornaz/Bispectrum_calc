922ba90117f8b60e6911d1ddd1814979db9f7ff1
Karin Fornazier <karin.fornazier@gmail.com>
2019-07-16 12:46:30 +0100

    Script corrected - no more green maps ;)

---
+++ Sum_map_Foregrounds_Flask/sumflaskforegroundspytfits2.py
@@ -0,0 +1,52 @@
+# encoding: utf-8
+##############################################################################
+##  algorithm for summing up two different outputs maps                 #####
+##  Flask                                                 
+##                                                                       #####
+##  Foregrounds 
+## Authors: Karin Fornazier
+## Email: karin.fornazier@gmail.com
+## Supervisor: F.B. Abdalla
+## Latest Version June 2019
+###########################################################################
+###########################################################################
+
+import numpy as np
+import healpy as hp
+import ConfigParser
+from astropy.io import fits as pyfits
+import matplotlib.pyplot as plt
+import sys
+
+#21cm from Flask
+#decrescente
+#mapas_21 = np.zeros((30,786432))
+#for i in range(1,30):
+#    a=30-i
+#    mapas_21[a,:]= hp.read_map('delta-f1z' + str(i) + '.fits')
+#    print "lendo-->", a
+
+
+#crescente
+mapas_21 = np.zeros((30,786432))
+for i in range(1,31):
+    a=i-1
+    mapas_21[a,:]= hp.read_map('delta-f1z' + str(i) + '.fits')
+    print "lendo-->", a
+
+pyfits.writeto('delta_flask_21_cube.fits', mapas_21)
+print "salvei pyfits 21"
+hp.mollview(mapas_21[0,:])
+
+print "salvei flask maps"
+
+# Foregrounds Maps
+
+mapas_foreground = pyfits.getdata("foreground_cube_Nside256.fits")
+print (mapas_foreground[0,:])
+mapas_final = mapas_foreground + mapas_21
+pyfits.writeto('cube_finalmap_delta_foregrounds.fits', mapas_final)
+hp.mollview(mapas_final[0,:])
+data = pyfits.getdata("cube_finalmap_delta_foregrounds.fits")
+plt.show()
+
